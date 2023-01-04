7d5e77bb017a76f619ec26014037f7f70eef0d58
Stephen McPhee <stephen.t.mcphee@gmail.com>
2023-01-04 12:42:25 -0700

    Initial commit

--- if_statements.rb
+++ if_statements.rb
@@ -46,6 +46,12 @@ num_quarters = 0
 puts "I have enough money for a gumball"
 puts "I don't have enough money for a gumball"
 
+num_quarters = 0
+if num_quarters >= 2 
+  puts "I have enough money for a gumball"
+  else puts "I don't have enough money for a gumball"
+end
+
 
 #####################
 # Using the variables defined below, write code that will tell you
@@ -63,3 +69,12 @@ puts "I don't have enough money for a gumball"
 
 cups_of_flour = 1
 has_sauce = true
+if cups_of_flour <= 1 && has_sauce == true
+  puts "I cannot make pizza"
+elsif cups_of_flour <= 1 && has_sauce == false
+  puts "I cannot make pizza"
+elsif cups_of_flour > 1 && has_sauce == false
+  puts "I cannot make pizza"
+else 
+  puts "I can make pizza"
+end
